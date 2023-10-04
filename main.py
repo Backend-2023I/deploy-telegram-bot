58e79f6dbd06c6d7e123369b779ddd2f006d96a1
JavohirJalilov <javohirjaliloff@gmail.com>
2023-10-04 14:59:39 +0500

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+import os 
+from flask import Flask, request
+
+TOKEN=os.environ["TOKEN"]
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return "DEPLOYMENT"
+
+if __name__ == "__main__":
+    app.run(debug=True)
